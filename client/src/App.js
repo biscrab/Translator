56656a5dae22d93c5c7b688729125463af9bb233
biscrab <biscrab@naver.com>
2021-10-07 09:00:13 +0900

    t

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as S from './styled/App'
+
+function App() {
+  return (
+    <>
+    <S.Text placeholder="번역할 내용을 입력해 주세요">
+  
+    </S.Text>
+    <S.Text>
+      
+    </S.Text>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/styled/App.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Text = styled.textarea`
+    padding: 10px;
+    resize: none;
+    height: 300px;
+    width: 500px;
+    outline: 0;
+    border: 2px solid royalblue;
+`
